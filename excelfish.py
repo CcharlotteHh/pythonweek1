82ff6791f4af6199270224c7dd578276a3becd62
CcharlotteHh <210547@student.glu.nl>
2022-10-06 13:40:23 +0200

    added things for fish assignment

---
+++ excelfish.py
@@ -0,0 +1,4 @@
+import pyautogui #import pyautogui
+
+myScreenshot = pyautogui.screenshot() #maakt myScreenshot aan en geeft een waarde mee 
+myScreenshot.save(r'C:\xampp\htdocs\p5\python week 1\data_to-fish') #pad waar myScreenShot moet worden opgeslagen
